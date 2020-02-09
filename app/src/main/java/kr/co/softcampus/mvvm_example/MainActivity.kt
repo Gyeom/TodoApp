3a8c5fece6446d0f5218d2739843bc59d8b3a20e
gyeom <koreatech93@naver.com>
2020-02-09 23:48:48 +0900

    Migrate TodoApp to Git

---
+++ app/src/main/java/kr/co/softcampus/mvvm_example/MainActivity.kt
@@ -0,0 +1,28 @@
+package kr.co.softcampus.mvvm_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kr.co.softcampus.mvvm_example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        val viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
+        binding.viewModel = viewModel
+
+    }
+}
